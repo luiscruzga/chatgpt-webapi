571599e34c2e96f92a0b02f2c5a258ae385422e0
Luis Cruz <lcruzgar@emeal.nttdata.com>
2023-01-10 18:20:46 -0300

    feat: change express by fastify

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import showdown from 'showdown';
-import Express from 'express';
-import bodyParser from 'body-parser';
+import fastify from 'fastify';
 import ChatGPT from './chatgpt.js';
 const AUTH_KEY = process.env.AUTH_KEY;
 const PORT = process.env.PORT||8080;
 const OPENAI_SESSION_TOKEN = process.env.OPENAI_SESSION_TOKEN;
 
-const app = Express();
-app.use(bodyParser.urlencoded({limit: '240mb', extended: true})); 
-app.use(bodyParser.json({limit: '240mb'})); 
-app.use(bodyParser.raw({type: 'application/octet-stream'}));
-app.use((req, res, next) => {
-  res.setTimeout(120000, () => {
-    console.log('Request has timed out.');
-    res.status(408).json({ error: 'Request has timed out!' });
-  });
-  next();
-});
+const app = fastify();
+app.server.setTimeout(120000);
 
 if (!AUTH_KEY){
   console.warn("Auth key not set! Everyone can access this instance.");
@@ -30,14 +20,12 @@ const escapeRegExp = (string) => {
 
 const replaceAll = (str, term, replacement) => str.replace(new RegExp(escapeRegExp(term), 'g'), replacement).trim();
 
-const diffMinutes = (dt2, dt1) => {
+const diffSeconds = (dt2, dt1) => {
   let diff =(dt2.getTime() - dt1.getTime()) / 1000;
-  diff /= 60;
   return Math.abs(Math.round(diff));
 }
 
 app.post('/chat', async (req, res) => {
-  res.contentType('application/json');
   try {
     const { sessiontoken } = req.headers;
     const { authKey, message, conversationId, formatToHtml } = req.body;
@@ -45,19 +33,20 @@ app.post('/chat', async (req, res) => {
     if (!sessiontoken) return res.status(403).json({ error: 'No sessiontoken specified!' });
     if (!message) throw "No message specified!";
     if (AUTH_KEY && authKey !== AUTH_KEY) throw "Invalid key!";
-    
+    const startDate = new Date();
+
     // Init session
     const bot = {};
     bot.instance = new ChatGPT(sessiontoken);
     await bot.instance.waitForReady();
     console.log('GPTChat init');
-    
-    const startDate = new Date();
     console.log("Request", message, conversationId);
 
     let response;
     if (conversationId) response = await bot.instance.ask(message, conversationId);
     else response = await bot.instance.ask(message);
+
+    if (!response) throw "Failed to send the question. Please try again later.";
     console.log('Response length', response.length);
 
     bot.instance.disconnect();
@@ -69,12 +58,12 @@ app.post('/chat', async (req, res) => {
 `)
       responseFormat = converter.makeHtml(responseFormat);
       const endDate = new Date();
-      const finalTime = diffMinutes(startDate, endDate);
+      const finalTime = diffSeconds(startDate, endDate);
       console.log('[Duracion Total]: ', finalTime);
       res.send({response, responseFormat});
     } else {
       const endDate = new Date();
-      const finalTime = diffMinutes(startDate, endDate);
+      const finalTime = diffSeconds(startDate, endDate);
       console.log('[Duracion Total]: ', finalTime);
       res.send({response:response});
     }
@@ -85,6 +74,6 @@ app.post('/chat', async (req, res) => {
   res.end();
 });
 
-app.listen(PORT, () => {
+app.listen({ port: PORT }, () => {
   console.log(`App listening on port ${PORT}`)
 });
